796a8c2f32406af189a932ecc32dcbf7508799a1
2019331101-Tanjumul <crackerroot4@gmail.com>
2024-04-11 03:24:08 +0600

    vector using resize option

---
+++ STL/vector2.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){ 
+
+vector<int>v {2,3,4,5};
+
+v.resize(10); 
+
+for(int i=0; i<v.size(); i++){ 
+    cout<<v[i]<<" "; 
+    //cout<<endl; 
+}
+
+
+}
