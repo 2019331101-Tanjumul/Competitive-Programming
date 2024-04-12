cc2d3aa31f713a8607df8b57b523340defe98407
2019331101-Tanjumul <crackerroot4@gmail.com>
2024-04-13 00:27:49 +0600

    using auto instade of any variable type in loop

--- STL/vector5.cpp
+++ STL/vector5.cpp
@@ -4,9 +4,12 @@ int main (){
 
 vector <int> v = { 22 ,23 , 44, 56, 64, 44};
 
-for(int u : v ){ 
+for(auto  u : v ){ 
+
     cout<<u<<" "; 
 }
+//we can use the auto to use it like the best way to declaring the integer or the place holder on the loop
+
 
 cout<<endl; 
 
