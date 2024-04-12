6d22ba87fd2f9c6d5892607de99ba1df37a56d8f
2019331101-Tanjumul <crackerroot4@gmail.com>
2024-04-12 23:55:14 +0600

    can declare the vector like this

---
+++ STL/vector3.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main(){ 
+
+vector<int>v = {1,2,3,4,5,6,7,8}; 
+vector<int>temp; 
+
+temp=v; 
+
+cout<<temp.size() << " "; 
+
+for(int i =0 ; i<temp.size(); i++) cout<<temp[i]<<" "; 
+cout<<endl; 
+
+
+
+
+}
