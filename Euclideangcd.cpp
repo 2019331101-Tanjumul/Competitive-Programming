328ad9ca8644ed95bae92f052de230dfb0de777a
2019331101-Tanjumul <crackerroot4@gmail.com>
2024-09-28 17:18:24 +0600

    Euclid algorithm

---
+++ Euclideangcd.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int gcd(int a , int b ){ 
+
+if (b ==0 )
+{ 
+    return a; 
+
+}
+return gcd(b,a%b); 
+}
+
+int main (){ 
+
+int t; 
+cin>> t;
+
+while(t--){ 
+
+int a , b; 
+
+cin>>a>>b; 
+
+int ans = gcd(a , b); 
+cout<<ans<<endl;
+
+}
+
+
+
+
+}
