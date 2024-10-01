7259dcb7de84286a2007df088b8a82e5bf994e84
2019331101-Tanjumul <crackerroot4@gmail.com>
2024-10-02 03:23:26 +0600

    C++ learning : datatypes

---
+++ datatypes.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){ 
+
+//memory c/c++ 
+
+/*basic data types : integer , double and char
+
+data type significance integer : 4byte , 8 bit = 1 byte ; 
+
+data type secures the spaces in the memory for the significant type to address in the memory ! 
+variable : 
+int = integer , 
+int x ; // it will secure the space x in the memory ! 
+
+there is a concept of Lvalue and R value 
+
+*/
+int total_students; 
+total_students = 20;
+ 
+ printf("Total number of students : %d\n", total_students);
+// printf("NAN");
+
+total_students = total_students + 5; 
+
+
+cout << total_students<<endl;
+
+
+}
