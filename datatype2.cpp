90fe8e66651c4f361ac23021e2b3ff5050b4d227
2019331101-Tanjumul <crackerroot4@gmail.com>
2024-10-06 03:14:58 +0600

    2nd part of the datatype

---
+++ datatype2.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){ 
+
+
+int x;
+
+x = 10 ; 
+
+printf("Value of x is : %d\n",x); 
+
+double y = 5.6; 
+
+int z = y; 
+
+cout<<"The value of the referance in integer is : "<<z<<endl; 
+
+cout<<"The actual value is : "<<y<<endl;
+return 0; 
+
+
+
+
+}
