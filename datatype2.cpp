fe2dd55ff47e74bc9dd55647e6329ea279622324
2019331101-Tanjumul <crackerroot4@gmail.com>
2024-10-06 03:29:02 +0600

    typecasting on different values

--- datatype2.cpp
+++ datatype2.cpp
@@ -6,6 +6,7 @@ int main (){
 int x;
 
 x = 10 ; 
+int a = 5 , b =2 ; 
 
 printf("Value of x is : %d\n",x); 
 
@@ -13,12 +14,31 @@ double y = 5.6;
 
 int z = y; 
 
-cout<<"The value of the referance in integer is : "<<z<<endl; 
+double n = 5/2 ; 
 
-cout<<"The actual value is : "<<y<<endl;
+printf("Value of n is : %lf",n); 
+
+
+printf("\nvalue of y is %lf",y);
+
+//typecasting : we can treat someone with the specific value point that's the main reason to work with the type casting comes.
+
+double p = a / (double)b; //here we are declaring or casting the type of b as a double but it was actually a integer type you see !
+
+printf(" \nThe value of P for typecasting example will be : %lf", p); 
+//other way to do the typecast : 
+
+
+//double p = 1.0*a / b ; it will be typecasted 
+
+
+
+
 return 0; 
 
 
 
+/* Instructor Tanjumul ; 
+ Program class record number : 09 */
 
 }
