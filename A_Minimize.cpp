5b99488123c5a4a12538b292a2b1fba198131c47
2019331101-Tanjumul <crackerroot4@gmail.com>
2024-10-21 00:21:57 +0600

    Div4 upsolve practice

---
+++ A_Minimize.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+
+void solve (){ 
+
+int n , a; 
+cin>>n>>a; 
+
+long long int results =(a-n); 
+
+cout<<results<<endl; 
+
+}
+
+
+
+int main(){
+
+int t; 
+cin>> t; 
+
+while(t--){ 
+
+    solve(); 
+}
+return 0; 
+}
